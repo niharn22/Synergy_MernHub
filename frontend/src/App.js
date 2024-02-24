68e39e4f0c1b2e6006e91891aa9512a291319782
sameer240704 <sameergupta79711@gmail.com>
2024-02-24 14:49:00 +0530

    Completed setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { Route, Switch } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 
-function App() {
+import Register from "./components/authentication/Register";
+import Login from "./components/authentication/Login";
+import NavbarComponent from "./components/Navbar";
+import Dashboard from "./components/Dashboard";
+
+import "./App.css";
+
+import { useDispatch, useSelector } from "react-redux";
+import { getUser } from "./redux/actionCreators/authActionCreators";
+
+const App = () => {
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      dispatch(getUser());
+    }
+  }, [dispatch]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ToastContainer position="bottom-right" />
+
+      <Switch>
+        <Route exact path={"/"}>
+          <NavbarComponent />
+          <h1>Welcome to file management system</h1>
+        </Route>
+        <Route exact path="/login" component={() => <Login />}></Route>
+        <Route exact path="/signup" component={() => <Register />}></Route>
+        <Route path="/dashboard" component={Dashboard} />
+      </Switch>
     </div>
   );
-}
+};
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import App from "./App";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
+import "./index.css";
+
+//redux
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+
+// reducers
+import authReducer from "./redux/reducers/authReducer";
+import filefolderReducer from "./redux/reducers/filefolderReducer";
+
+const reducers = combineReducers({
+  auth: authReducer,
+  filefolders: filefolderReducer,
+});
+
+const store = createStore(
+  reducers,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
